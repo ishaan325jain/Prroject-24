fc1311e99ba2452cbd277fe30d4440cbfbfdc922
ishaan325jain <73381737+ishaan325jain@users.noreply.github.com>
2020-12-22 12:05:28 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,23 @@
+class Paper {
+  constructor(x, y,radius) {
+    var options = {
+      'isStatic'    : false ,
+      'restitution' : 0.3   ,
+      'density'     : 0.5   ,
+      'friction'    : 1.2      
+  }
+    this.body = Bodies.circle(x, y, radius/2, options);
+    this.radius = radius;
+   
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+      push();
+      translate(pos.x,pos.y);  
+
+    rectMode(CENTER); 
+    circle(0,0,this.radius,this.radius);
+    pop();
+   }
+}; 
